7cfec4b89e9af586a044a20601d9b2ab2734ec1d
Sanjay Venugopal <iamsanjaykrishnan@gmail.com>
2025-05-04 13:32:50 +0200

    removed all svelte app files

--- src/stores/imageStore.ts
+++
@@ -1,6 +0,0 @@
-// src/stores/imageStore.ts
-import { writable } from 'svelte/store';
-
-// This store will hold the list of image URLs
-export const imageUrls = writable<string[]>([]);
-export const files = writable<File[]>([]);
